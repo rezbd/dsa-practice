73f3164b3060ad1442ed19d15d29cac7b8b88e2a
rezwan <rezwan@yotech.ltd>
2024-03-08 16:02:14 +0600

    function to delete a node basics

---
+++ singly-linked-list/delete-node-basics.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int v)
+{
+    Node* newNode = new Node(v);
+    if(head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void print_list(Node *head)
+{
+    while(head != NULL)
+    {
+        cout<<head->val<<" ";
+        head = head->next;
+    }
+    cout<<endl;
+}
+
+void delete_node(Node* &head, int pos)
+{
+    Node* tmp = head;
+    for(int i=1; i<=pos-1; i++)
+    {
+        tmp = tmp->next;
+    }
+    Node* deleteNode = tmp->next;
+    tmp->next = tmp->next->next;
+    delete deleteNode;
+}
+
+int main()
+{
+    int val;
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while(true)
+    {
+        cin>>val;
+        if(val == -1) break;
+        insert_at_tail(head, tail, val);
+    }
+    print_list(head);
+    delete_node(head, 2);
+    print_list(head);
+    return 0;
+}
