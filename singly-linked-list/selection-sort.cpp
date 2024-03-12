96e431199b2995980802fa802972a53e0606c5bd
rezwan <rezwan@yotech.ltd>
2024-03-12 16:29:38 +0600

    in progress

---
+++ singly-linked-list/selection-sort.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        int val;
+        Node* next;
+
+        Node(int val)
+        {
+            this->val = val;
+            this->next = NULL
+        }
+};
+
+void insert_at_tail(Node* head, Node* tail, int v)
+{
+    Node* newNode = new Node(v);
+    if(head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void print_linked_list(Node* head)
+{
+    while(head != NULL)
+    {
+        cout<<head->val<<" ";
+        head = head->next;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    // in progress
+    int val;
+    Node* head = NULL;
+    Node* tail = NULL;
+    while(true)
+    {
+        cin>>val;
+        if(val == -1) break;
+        insert_at_tail(head);
+    }
+
+    print_linked_list(head);
+}
