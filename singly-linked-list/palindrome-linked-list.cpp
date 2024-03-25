1f382e50a0d1d0f52918fff4e06fcaf927e48b98
rezwan <rezwan@yotech.ltd>
2024-03-25 15:23:46 +0600

    to check if a singly linked list is palindrome

---
+++ singly-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        int val;
+        Node* next;
+
+        Node(int val)
+        {
+            this->val = val;
+            this->next = next;
+        }
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int val)
+{
+    Node* newNode = new Node(val);
+    if(head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void insert_at_head(Node* &head, int val)
+{
+    Node* newNode = new Node(val);
+    if(head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+    newNode->next = head;
+    head = newNode;
+}
+
+void print_linked_list(Node* head)
+{
+    while(head != NULL)
+    {
+        cout<<head->val<<" ";
+        head = head->next;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    int val;
+    Node* head = NULL;
+    Node* tail = NULL;
+    Node* newHead = NULL;
+    while(true)
+    {
+        cin>>val;
+        if(val == -1) break;
+        insert_at_tail(head, tail, val);
+        insert_at_head(newHead, val);
+    }
+    // print_linked_list(head);
+    // cout<<"reversed list:"<<endl;
+    // print_linked_list(newHead);
+    while(head != NULL)
+    {
+        cout<<head->val<<" "<<newHead->val<<endl;
+        if(head->val != newHead->val)
+        {
+            cout<<"Not palindrome"<<endl;
+            return 0;
+        }
+        head = head->next;
+        newHead = newHead->next;
+    }
+    cout<<"palindrome"<<endl;
+    return 0;
+}
